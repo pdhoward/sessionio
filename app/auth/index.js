70d88a2ae54bd2ab4ce56ea4f7fd56266d6330ca
pdhoward <patrick.howard@hotmail.com>
2017-07-10 12:11:22 -0400

    updates

--- app/auth/index.js
+++ app/auth/index.js
@@ -11,7 +11,7 @@ var TwitterStrategy  	= require('passport-twitter').Strategy;
 var User = require('../models/user');
 
 /**
- * Encapsulates all code for authentication 
+ * Encapsulates all code for authentication
  * Either by using username and password, or by using social accounts
  *
  */
@@ -50,12 +50,12 @@ var init = function(){
 	  }
 	));
 
-	// In case of Facebook, tokenA is the access token, while tokenB is the refersh token.
+	// In case of Facebook, tokenA is the access token, while tokenB is the refresh token.
 	// In case of Twitter, tokenA is the token, whilet tokenB is the tokenSecret.
 	var verifySocialAccount = function(tokenA, tokenB, data, done) {
 		User.findOrCreate(data, function (err, user) {
 	      	if (err) { return done(err); }
-			return done(err, user); 
+			return done(err, user);
 		});
 	};
 
@@ -65,5 +65,5 @@ var init = function(){
 
 	return passport;
 }
-	
+
 module.exports = init();
